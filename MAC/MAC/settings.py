15ed99e7bf7a9c522df0e56cce354d3429d44647
Abhinav <tiwariabhinavin@gmail.com>
2020-08-13 13:31:20 +0530

    Added apps and shop and laid basic pipeline and also configured database

--- MAC/MAC/settings.py
+++ MAC/MAC/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import  os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'shop.apps.ShopConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
--- MAC/shop/urls.py
+++ MAC/shop/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import  settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('shop/',include('shop.urls')),
-    path('blog/',include('blog.urls'))
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('', views.index,name="ShopHome"),
+    path('about/', views.about,name="AboutUs"),
+    path('contact/', views.contact,name="ContactUs"),
+    path('tracker/', views.tracker,name="TrackingSatus"),
+    path('search/', views.search,name="Search"),
+    path('productview/', views.productView,name="Search"),
+    path('checkout/', views.checkout,name="Checkout"),
+
+]
