16b4c5f67eeb794106a10afb4fb6d65a89ba4f59
Abhinav <tiwariabhinavin@gmail.com>
2020-08-23 10:08:27 +0530

    Added sliders for homepage and configured database

--- MAC/shop/views.py
+++ MAC/shop/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from . models import product
+import math
+Products= product.objects.all()
+print(Products)
 
 def index(request):
-    return render(request,'shop/index.html')
+
+    #n=len(Products)
+    #nSlides=int(n/4+math.ceil(n/4-n//4))
+
+    #allProds=[[Products,range(1,nSlides),nSlides],[Products,range(1,nSlides),nSlides]]
+    allProds=[]
+    catprods=product.objects.values('category','id')
+    cats={item['category'] for item in catprods} #Gets all categories
+    for cat in cats :
+        prod=product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = int(n / 4 + math.ceil(n / 4 - n // 4))
+        allProds.append([prod, range(1,nSlides), nSlides])
+
+    params={'allProds':allProds}
+    return render(request,'shop/index.html',params)
 def about(request):
-    return HttpResponse("We are at about")
+    return render(request,'shop/about.html')
 def contact(request):
     return HttpResponse("We are at contact")
 def tracker(request):
